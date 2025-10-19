2da75e1e6df618d2fbace2a2043f53824e227f98
Suyash Baoney <suyashbaoney58@gmail.com>
2025-10-19 16:36:06 +0530

    project setup

---
+++ Server-Side-Events-Demo/src/main/java/com/example/springboot/Server_Side_Events_Demo/ServerSideEventsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.Server_Side_Events_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerSideEventsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerSideEventsDemoApplication.class, args);
+	}
+
+}
